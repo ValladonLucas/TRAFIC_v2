a98b51954b9ee17a2f56b47c5ac6e40c76fce67d
Lucas Valladon <valladonlucas@gmail.com>
2024-07-23 10:41:19 -0400

    Updated Classification process

--- Classification/classification.py
+++ Classification/classification.py
@@ -1,4 +1,7 @@
+# Add the parent directory to the system path
+import sys
 import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import vtk
 import torch
 import numpy as np
